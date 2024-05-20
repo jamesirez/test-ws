c08362414ef68d9cede11c49c4ab2a173f4229c0
Thanh Hoang <dinhthanh2000a@gmail.com>
2024-05-20 18:00:07 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io("https://ton-bot-api.com");
 
 function App() {
+  const [swapData, setSwapData] = useState(null);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to socket server");
+    });
+
+    socket.on("SWAP_COMPLETED", (data) => {
+      setSwapData(data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from socket server");
+    });
+
+    // Clean up the effect
+    return () => {
+      socket.off("connect");
+      socket.off("SWAP_COMPLETED");
+      socket.off("disconnect");
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +44,19 @@ function App() {
         >
           Learn React
         </a>
+        {swapData && (
+          <div>
+            <h2>Swap Completed:</h2>
+            <p>Type: {swapData.type}</p>
+            <p>UserId: {swapData?.userId}</p>
+            <p>TelegramId: {swapData?.telegramId}</p>
+            <p>Token In: {swapData.tokenIn}</p>
+            <p>Amount In: {swapData.amountIn}</p>
+            <p>Token Out: {swapData.tokenOut}</p>
+            <p>Amount Out: {swapData.amountOut}</p>
+            <p>Transaction Hash: {swapData.hash}</p>
+          </div>
+        )}
       </header>
     </div>
   );
